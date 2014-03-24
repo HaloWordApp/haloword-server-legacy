0461de2acdce7276f07cdc2deead3c5a64a15b07
Xhacker Liu <liu.dongyuan@gmail.com>
2014-03-23 23:49:50 -0700

    Add redis config. Use redis-py.

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask
-from flask_redis import Redis
+import redis
 
 import requests
 
 app = Flask(__name__)
-REDIS_URL = "redis://localhost:6379/0"
-redis_store = Redis(app)
+redis_store = redis.StrictRedis(host='localhost', port=6233, db=0)
 
 API_URL = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/{}?key=6d9366e8-dc95-4b44-931a-ff90bc8f96bd"
 
 
-@app.route("/<word>")
+@app.route("/webster/query/<word>")
 def index(word):
     if not redis_store.exists(word):
+        print "Not cached:", word
         text = requests.get(API_URL.format(word)).text
         redis_store.set(word, text)
         return text
